9f9c25d24c52f86527c70c33a3ba71467fe6d4ff
doniagung <agungdoni823@gmail.com>
2024-03-04 14:29:39 +0700

    dynamic

--- dart_project/bin/01_map.dart
+++ dart_project/bin/01_map.dart
@@ -1,6 +1,6 @@
 void main() {
     Map<String, dynamic> data = {
-       'url': https://unisnu.ac.id/’,
+       'url': 'https://unisnu.ac.id/',
         'topic': 'Flutter Engineering',
     };
     print(data);
